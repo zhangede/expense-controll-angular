7767ff1e1215cd3054922a9b84d917c089c16a99
Feng <zhang7777@gmail.com>
2023-03-19 15:20:52 +0100

    add signin pages

--- src/app/pages/signin/signin.module.ts
+++ src/app/pages/signin/signin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button'; 
 
 const routes: Routes = [{
   path: '', component: SigninComponent
@@ -14,6 +16,8 @@ const routes: Routes = [{
   ],
   imports: [
     CommonModule,
+    MatInputModule,
+    MatButtonModule,
     RouterModule.forChild(routes)
   ]
 })
